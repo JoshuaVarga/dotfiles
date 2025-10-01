6eb441da8b6d8580c39536110931a8c1ac071b90
Joshua Varga <joshua.varga@prolucid.ca>
2025-10-01 15:22:06 -0400

    add wezterm, nushell, starship configs

---
+++ dot_wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require "wezterm"
+
+local config = wezterm.config_builder()
+
+-- Default to Nushell
+config.default_prog = { "nu.exe" }
+
+-- Launch menu with labels
+config.launch_menu = {
+  { label = "Nushell",   args = { "nu.exe" } },
+  { label = "PowerShell", args = { "pwsh.exe" } },
+  { label = "CMD",       args = { "cmd.exe" } },
+}
+
+-- Apearance
+config.color_scheme = "Tokyo Night Storm"
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.enable_tab_bar = false
+config.window_background_opacity = 0.9
+
+return config
